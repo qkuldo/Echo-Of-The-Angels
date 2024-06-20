33ee7a8c0547627e67bfe826754842a2e11eabe3
qk <teoa52269@gmail.com>
2024-06-20 13:28:48 +0800

    Add files via upload
    Updated particle.py to give separate textures for each particle

--- the echo of the angels/classes/particle.py
+++ the echo of the angels/classes/particle.py
@@ -2,18 +2,19 @@ import pygame
 import copy
 class Particle:
 	"""A class to define individual particles"""
-	def __init__(self, pos=[0,0], velocity_x=1, velocity_y=1, lifetime=400):
+	def __init__(self, texture,pos=[0,0], velocity_x=1, velocity_y=1, lifetime=400):
 		self.pos = pos
 		self.velocity_x = velocity_x
 		self.velocity_y = velocity_y
 		self.lifetime = lifetime
+		self.texture = texture
 	def move(self, clock):
 		self.pos[0] += self.velocity_x
 		self.pos[1] += self.velocity_y
 		self.lifetime -= clock.get_time()
 class ParticleGroup:
 	"""A class to define groups of particles"""
-	def __init__(self, texture, pos, velocity_x, velocity_y, lifetime):
+	def __init__(self, texture, pos, velocity_x, velocity_y, lifetime,fade=False):
 		self.particlelist = []
 		self.texture = texture
 		self.pos = pos
@@ -21,7 +22,7 @@ class ParticleGroup:
 		self.velocity_y = velocity_y
 		self.lifetime = lifetime
 	def spawn_particle(self):
-		self.particlelist.append(Particle(self.pos,self.velocity_x,self.velocity_y,self.lifetime))
+		self.particlelist.append(Particle(copy.copy(self.texture),self.pos,self.velocity_x,self.velocity_y,self.lifetime))
 	def update_group(self, clock):
 		for particle in self.particlelist:
 			particle.move(clock)
@@ -29,7 +30,7 @@ class ParticleGroup:
 				self.particlelist.remove(particle)
 	def draw(self, screen):
 		for particle in self.particlelist:
-			screen.blit(self.texture, particle.pos)
+			screen.blit(particle.texture, particle.pos)
 	def setup(self, screen, clock):
 		self.update_group(clock)
 		self.draw(screen)
